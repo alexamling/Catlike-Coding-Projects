cd4be4d37f91e5a37433ffab69e487b0a5b9763c
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:39:23 -0400

    Basic Creation of Graph Points
    Placement and scaling of points along a desired range.

--- Catlike Coding - Graphs/Assets/Graph.cs
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -7,11 +7,11 @@ public class Graph : MonoBehaviour {
     public Transform pointPrefab;
 
 	void Start () {
-        Transform point = Instantiate(pointPrefab);
-        point.localPosition = transform.right;
-        
-        point = Instantiate(pointPrefab);
-        point.localPosition = transform.right * 2f;
+        for (int i = 0; i < 10; i++){
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = transform.right * ((i + .5f) / 5f - 1f); // center cubes and place between (-1,1)
+            point.localScale = Vector3.one / 5f;
+        }
     }
 	
 
