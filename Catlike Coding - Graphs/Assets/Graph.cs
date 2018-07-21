05c21c1d2845553614c138cf3f8f43f6c602158e
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:57:15 -0400

    Custom Shader
    Custom shader that changes object albedo based off of x and y axis position

--- Catlike Coding - Graphs/Assets/Graph.cs
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -20,6 +20,7 @@ public class Graph : MonoBehaviour {
             position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform);
         }
     }
 	
