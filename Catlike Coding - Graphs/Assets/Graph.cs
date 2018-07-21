5cf450b2af1f76f606089ffad9655e7235fb2883
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:44:23 -0400

    Simple Parabolic Graph
    Simple representation of a points on a parabola.

--- Catlike Coding - Graphs/Assets/Graph.cs
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -7,10 +7,16 @@ public class Graph : MonoBehaviour {
     public Transform pointPrefab;
 
 	void Start () {
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position;
+        position.z = 0f;
+
         for (int i = 0; i < 10; i++){
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = transform.right * ((i + .5f) / 5f - 1f); // center cubes and place between (-1,1)
-            point.localScale = Vector3.one / 5f;
+            position.x = (i + .5f) / 5f - 1f; // center cubes and place between (-1,1)
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
         }
     }
 	
