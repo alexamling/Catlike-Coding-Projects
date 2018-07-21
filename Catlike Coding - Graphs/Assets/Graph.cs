603b9fa0ee50c3f931b661d1d9c0376702af52ab
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:49:56 -0400

    Resolution Slider and Camera Adjustments
    Added a resolution slider that changes the number, scale, and positioning of blocks to keep them within (-1,1)

--- Catlike Coding - Graphs/Assets/Graph.cs
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Graph : MonoBehaviour {
 
     public Transform pointPrefab;
+    [Range(10,100)]
+    public int resolution = 10;
 
 	void Start () {
-        Vector3 scale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position;
         position.z = 0f;
 
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < resolution; i++){
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + .5f) / 5f - 1f; // center cubes and place between (-1,1)
+            position.x = (i + .5f) * step - 1f; // center cubes and place between (-1,1)
             position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
