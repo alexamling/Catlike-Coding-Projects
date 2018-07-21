0179de865a52852926b225340f13f9b0ce2e9137
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:32:33 -0400

    Basic Graph Project Setup
    Basic setup including object prefabs for representing the graph, a script for the graph and an empty to hold it.

---
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+
+	void Start () {
+        Transform point = Instantiate(pointPrefab);
+        point.localPosition = transform.right;
+        
+        point = Instantiate(pointPrefab);
+        point.localPosition = transform.right * 2f;
+    }
+	
+
+	void Update () {
+		
+	}
+}
