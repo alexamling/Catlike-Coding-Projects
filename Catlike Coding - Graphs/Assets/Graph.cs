f60471efb2c9d669baad5c4df1291fca3b6e13d4
Alexander Amling <aja7073@rit.edu>
2018-07-21 17:10:01 -0400

    Completed Proejct: Graphs
    Comepleted project implements a sine wave that moves over time and adjusts object color accordingly.

--- Catlike Coding - Graphs/Assets/Graph.cs
+++ Catlike Coding - Graphs/Assets/Graph.cs
@@ -8,16 +8,23 @@ public class Graph : MonoBehaviour {
     [Range(10,100)]
     public int resolution = 10;
 
+    Transform[] points;
+
 	void Start () {
+        points = new Transform[resolution];
+
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 position;
+        position.y = 0f;
         position.z = 0f;
 
         for (int i = 0; i < resolution; i++){
             Transform point = Instantiate(pointPrefab);
+            points[i] = point;
+
             position.x = (i + .5f) * step - 1f; // center cubes and place between (-1,1)
-            position.y = position.x * position.x;
+
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform);
@@ -26,6 +33,13 @@ public class Graph : MonoBehaviour {
 	
 
 	void Update () {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * position.x + Time.time);
+            point.localPosition = position;
+        }
 		
 	}
 }
