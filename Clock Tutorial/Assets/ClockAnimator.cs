19ec2d08cb644a765a0da7a396f2366ed68b1855
Alexander Amling <aja7073@rit.edu>
2018-07-21 16:22:12 -0400

    Addition of Old Proejcts and Creation of Next Project
    I added two fo the completed catlike coding projects that I did back in 2016.
    
    I created a new project to start working on the graph tutorial.

---
+++ Clock Tutorial/Assets/ClockAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	private const float
+		hoursToDegrees = 360f / 12f,
+		minutesToDegrees = 360f / 60f,
+		secondsToDegrees = 360f / 60;
+
+	public Transform hours, minutes, seconds;
+
+	public bool analog;
+
+	private void Update () {
+		if (analog) {
+			TimeSpan timespan = DateTime.Now.TimeOfDay;
+			hours.localRotation = 
+				Quaternion.Euler (0, 0, (float)timespan.TotalHours * -hoursToDegrees);
+			minutes.localRotation = 
+				Quaternion.Euler (0, 0, (float)timespan.TotalMinutes * -minutesToDegrees);
+			seconds.localRotation = 
+				Quaternion.Euler (0, 0, (float)timespan.TotalSeconds * -secondsToDegrees);
+		} else {
+			DateTime time = DateTime.Now;
+			hours.localRotation = 
+			Quaternion.Euler (0, 0, time.Hour * -hoursToDegrees);
+			minutes.localRotation = 
+			Quaternion.Euler (0, 0, time.Minute * -minutesToDegrees);
+			seconds.localRotation = 
+			Quaternion.Euler (0, 0, time.Second * -secondsToDegrees);
+		}
+	}
+}
