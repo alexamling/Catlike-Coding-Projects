639af8f0c951370cd0ca2cd274e8ac98194e3cf1
Alexander Amling <aja7073@rit.edu>
2018-07-22 12:27:20 -0400

    Basic setup for the framerate project
    Creation of a scene that rapidly and endlessly spawns "protons" and "Neutrons" that all gravitate towards the origin.
    
    This project investigates ways to moniter, maximize, and otherwise work with framerate.

---
+++ Catlike Coding - Framerate/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+	// Use this for initialization
+	void Awake () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        body.AddForce(transform.localPosition * -attractionForce);
+	}
+}
---
+++ Catlike Coding - Framerate/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+
+    float timeSinceLastSpawn;
+
+    public Nucleon[] nucleonPrefabs;
+
+	// Use this for initialization
+	void Awake() {
+		
+	}
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab, gameObject.transform);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
