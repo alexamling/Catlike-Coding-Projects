0fc87d8d584b80554c4c481cbafdd4df3c1f0252
Alexander Amling <aja7073@rit.edu>
2018-07-22 22:41:13 -0400

    Added FPS readout
    Added a UI asset that reads the current FPS rate.

---
+++ Catlike Coding - Framerate/Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+    
+    public int FPS { get; private set; }
+    
+	void Update () {
+        FPS = (int)(1 / Time.unscaledDeltaTime);
+	}
+}
---
+++ Catlike Coding - Framerate/Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour {
+
+    public Text fpsLabel;
+
+    FPSCounter fpsCounter;
+
+	void Awake () {
+        fpsCounter = GetComponent<FPSCounter>();
+	}
+
+	void Update () {
+        fpsLabel.text = Mathf.Clamp(fpsCounter.FPS, 0, 99).ToString();
+	}
+}
