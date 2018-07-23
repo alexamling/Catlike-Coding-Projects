101688c6c7f9dddf076376ff13b17511df67d80d
Alexander Amling <aja7073@rit.edu>
2018-07-22 23:10:41 -0400

    Changed Display to Read Min, Max, and Average FPS
    Changed the FPS readout to read min max and average FPS, calculated over a variable number of frames.

--- Catlike Coding - Framerate/Assets/Scripts/FPSCounter.cs
+++ Catlike Coding - Framerate/Assets/Scripts/FPSCounter.cs
@@ -1,10 +1,64 @@
 ﻿using UnityEngine;
 
 public class FPSCounter : MonoBehaviour {
-    
-    public int FPS { get; private set; }
-    
-	void Update () {
-        FPS = (int)(1 / Time.unscaledDeltaTime);
-	}
+
+    public int frameRange = 60;
+
+    public int AverageFPS { get; private set; }
+    public int MinFPS { get; private set; }
+    public int MaxFPS { get; private set; }
+
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    void Update() {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange)
+        {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS()
+    {
+        int sum = 0;
+        int min = int.MaxValue;
+        int max = 0;
+        for(int i = 0; i < frameRange; i++)
+        {
+            int fps = fpsBuffer[i];
+            sum += fps;
+            if (fps > max)
+            {
+                max = fps;
+            }
+            if (fps < min)
+            {
+                min = fps;
+            }
+            AverageFPS = sum / frameRange;
+            MaxFPS = max;
+            MinFPS = min;
+        }
+    }
 }
--- Catlike Coding - Framerate/Assets/Scripts/FPSDisplay.cs
+++ Catlike Coding - Framerate/Assets/Scripts/FPSDisplay.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(FPSCounter))]
 public class FPSDisplay : MonoBehaviour {
 
-    public Text fpsLabel;
+    public Text MaxFPSLabel, AverageFPSLabel, MinFPSLabel;
 
     FPSCounter fpsCounter;
 
@@ -12,7 +12,24 @@ public class FPSDisplay : MonoBehaviour {
         fpsCounter = GetComponent<FPSCounter>();
 	}
 
-	void Update () {
-        fpsLabel.text = Mathf.Clamp(fpsCounter.FPS, 0, 99).ToString();
-	}
+    static string[] stringsFrom00To99 =
+    {
+        "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+        "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+        "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+        "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+        "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+        "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+        "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+        "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+    };
+
+	void Update ()
+    {
+        MaxFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.MaxFPS, 0, 99)];
+        AverageFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.AverageFPS, 0, 99)];
+        MinFPSLabel.text = stringsFrom00To99[Mathf.Clamp(fpsCounter.MinFPS, 0, 99)];
+    }
 }
