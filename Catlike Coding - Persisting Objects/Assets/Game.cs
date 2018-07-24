33c8795fff4dd07d5f115acd3118a053ff739f87
Alexander Amling <aja7073@rit.edu>
2018-07-24 18:53:48 -0400

    Basic Setup for Persisting Object Project
    Setup a project where a specific key press will spawn a prefab.

---
+++ Catlike Coding - Persisting Objects/Assets/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public Transform prefab;
+
+    public KeyCode createKey = KeyCode.C;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(createKey))
+        {
+            Instantiate(prefab);
+        }
+	}
+}
