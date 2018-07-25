e1f3a34f732df4302b5b204dd3850a37edddbf2a
Alexander Amling <aja7073@rit.edu>
2018-07-24 20:23:20 -0400

    Saving and Loading
    implemented basic file reading and writing

--- Catlike Coding - Persisting Objects/Assets/Game.cs
+++ Catlike Coding - Persisting Objects/Assets/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
@@ -8,11 +9,35 @@ public class Game : MonoBehaviour {
 
     public KeyCode createKey = KeyCode.C;
     public KeyCode newGameKey = KeyCode.N;
-    
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    List<Transform> objects;
+    string savePath;
+
+    void Awake()
+    {
+        objects = new List<Transform>();
+
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
 	void Update () {
         if (Input.GetKeyDown(createKey))
         {
             CreateObject();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            Save();
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            Load();
         }
 	}
 
@@ -22,5 +47,52 @@ public class Game : MonoBehaviour {
         t.localPosition = Random.insideUnitSphere * 5f;
         t.rotation = Random.rotation;
         t.localScale = Vector3.one * Random.Range(.1f, 1f);
+        objects.Add(t);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    void Save()
+    {
+        using (
+            BinaryWriter writer = new BinaryWriter(File.Open(savePath, FileMode.Create))
+            ){
+            writer.Write(objects.Count);
+            for(int i = 0; i < objects.Count; i++)
+            {
+                Transform t = objects[i];
+                writer.Write(t.localPosition.x);
+                writer.Write(t.localPosition.y);
+                writer.Write(t.localPosition.z);
+            }
+        }
+    }
+
+    void Load()
+    {
+        BeginNewGame();
+        using (
+            BinaryReader reader = new BinaryReader(File.Open(savePath, FileMode.Open))
+            )
+        {
+            int count = reader.ReadInt32();
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 p;
+                p.x = reader.ReadSingle();
+                p.y = reader.ReadSingle();
+                p.z = reader.ReadSingle();
+                Transform t = Instantiate(prefab);
+                t.localPosition = p;
+                objects.Add(t);
+            }
+        }
     }
 }
