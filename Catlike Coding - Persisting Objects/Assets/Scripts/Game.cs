218a72ca190638f028ca317f004e5b405661101a
Alexander Amling <aja7073@rit.edu>
2018-07-24 21:20:29 -0400

    Major Saving and Loading Improvments
    By abstracting the reading and writing to a gamereader and gamewriter class, and creating a persisting object class with a persisting storage to hold it, it is now much easer to save and load objects.

---
+++ Catlike Coding - Persisting Objects/Assets/Scripts/Game.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Game : PersistableObject {
+
+    public PersistableObject prefab;
+    public PersistentStorage storage;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+    public KeyCode loadKey = KeyCode.L;
+
+    List<PersistableObject> objects;
+
+    void Awake()
+    {
+        objects = new List<PersistableObject>();
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(saveKey))
+        {
+            storage.Save(this);
+        }
+        else if (Input.GetKeyDown(loadKey))
+        {
+            BeginNewGame();
+            storage.Load(this);
+        }
+	}
+
+    void CreateObject()
+    {
+        PersistableObject o = Instantiate(prefab);
+        Transform t = o.transform;
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.rotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(.1f, 1f);
+        objects.Add(o);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(objects.Count);
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].Save(writer);
+        }
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        int count = reader.ReadInt();
+        for (int i = 0; i < count; i++)
+        {
+            PersistableObject o = Instantiate(prefab);
+            o.Load(reader);
+            objects.Add(o);
+        }
+    }
+}
---
+++ Catlike Coding - Persisting Objects/Assets/Scripts/PersistentStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentStorage : MonoBehaviour {
+
+    string savePath;
+
+    void Awake()
+    {
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+	
+    public void Save(PersistableObject o)
+    {
+        using (BinaryWriter writer = new BinaryWriter(File.Open(savePath, FileMode.Create)))
+        {
+            o.Save(new GameDataWriter(writer));
+        }
+    }
+
+    public void Load(PersistableObject o)
+    {
+        using (BinaryReader reader = new BinaryReader(File.Open(savePath, FileMode.Open)))
+        {
+            o.Load(new GameDataReader(reader));
+        }
+    }
+}
